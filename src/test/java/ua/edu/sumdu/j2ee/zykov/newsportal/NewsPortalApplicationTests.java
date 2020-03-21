0782807e5117d871029a95cf039800b80bf2da8b
Maxuha <zykovmaksym@gmail.com>
2020-03-21 10:40:26 +0200

    Initial commit

---
+++ src/test/java/ua/edu/sumdu/j2ee/zykov/newsportal/NewsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.j2ee.zykov.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
