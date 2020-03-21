1f2e0062f3c9ea6fa88418243e0a9cf4d5bad1ec
Maxuha <zykovmaksym@gmail.com>
2020-03-21 11:01:56 +0200

    Add network class

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/util/Network.java
@@ -0,0 +1,40 @@
+package ua.edu.sumdu.j2ee.zykov.util;
+
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class Network {
+    @Value("${data.news.token}")
+    private static String token;
+    private static OkHttpClient client = new OkHttpClient();
+
+    public static String getResponse(String url, Map<String, String> parameters) throws IOException {
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+            urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
+        }
+        url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .header("X-Api-Key", token)
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public static InputStream getImageInputStream(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().byteStream();
+    }
+}
