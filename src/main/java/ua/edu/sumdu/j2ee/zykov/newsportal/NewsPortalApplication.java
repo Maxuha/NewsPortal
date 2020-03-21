0c1e718fb7c70e19d7985567dd35a9016716ce8e
Maxuha <zykovmaksym@gmail.com>
2020-03-21 11:52:43 +0200

    Add base package

--- src/main/java/ua/edu/sumdu/j2ee/zykov/newsportal/NewsPortalApplication.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/newsportal/NewsPortalApplication.java
@@ -2,7 +2,9 @@ package ua.edu.sumdu.j2ee.zykov.newsportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "ua.edu.sumdu.j2ee.zykov")
 @SpringBootApplication
 public class NewsPortalApplication {
 
