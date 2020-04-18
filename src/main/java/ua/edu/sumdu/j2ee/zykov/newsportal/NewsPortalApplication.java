3e1c96a75edfa984f53462c79670be3a415a9c12
Maxuha <zykovmaksym@gmail.com>
2020-04-18 14:21:36 +0300

    Add cash manager: updating cash every 5 minutes

--- src/main/java/ua/edu/sumdu/j2ee/zykov/newsportal/NewsPortalApplication.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/newsportal/NewsPortalApplication.java
@@ -1,9 +1,17 @@
 package ua.edu.sumdu.j2ee.zykov.newsportal;
 
+import com.google.common.cache.CacheBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.concurrent.TimeUnit;
 
 @ComponentScan(basePackages = "ua.edu.sumdu.j2ee.zykov")
 @SpringBootApplication
@@ -14,4 +22,18 @@ public class NewsPortalApplication {
         SpringApplication.run(NewsPortalApplication.class, args);
     }
 
+    @Bean("newsCacheManager")
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager() {
+            @Override
+            protected Cache createConcurrentMapCache(String name) {
+                return new ConcurrentMapCache(
+                        name,
+                        CacheBuilder.newBuilder()
+                                .expireAfterWrite(300, TimeUnit.SECONDS)
+                                .build().asMap(),
+                        false);
+            }
+        };
+    }
 }
